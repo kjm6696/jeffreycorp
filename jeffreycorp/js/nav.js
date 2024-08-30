42dbf96cd4c10d1e36122afa664ef1d2837d513f
kjm6696 <122549649+kjm6696@users.noreply.github.com>
2024-08-30 15:30:05 -0400

    fix

--- jeffreycorp/js/nav.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById('mobileNav');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-}
